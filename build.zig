18f2e00e994d8ca0e3cd446cb87260858b45d25e
Somnath Nandi <somnathnandi368@gmail.com>
2026-01-23 11:52:26 +0530

    updating and adding docs

--- build.zig
+++ build.zig
@@ -57,7 +57,7 @@ pub fn build(b: *std.Build) void {
    const install_docs = b.addInstallDirectory(.{
         .source_dir = lib.getEmittedDocs(),
         .install_dir = .prefix,
-        .install_subdir = "v0.3.0",
+        .install_subdir = "docs",
     });
 
     const docs_step = b.step("docs", "Install docs into zig-out/docs");
